eb597e0c366f5f91030d7f76905dda3040280435
igor <igorkaktus@gmail.com>
2025-01-27 17:20:35 +0000

    updated our store and optimized it

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,35 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { binanceApiSlice } from './slices/binanceApiSlice';
-import { cryptoApiSlice } from './slices/cryptoApiSlice';
-import { marketDataApi } from './slices/marketDataSlice';
-import { coinDataApi } from './slices/coinDataApi';
-import {сoinsListWithMarketDataApiScile} from './slices/сoinsListWithMarketDataApiScile';
 import userReducer from './slices/usersSlice';
-import { сoinTickersByIdApiSlice } from './slices/сoinTickersByIdApiScile';
 import { coinsApiSlice } from './slices/coinsApiSlice';
 import {globalApiSlice} from './slices/globalApiSlice'
 
 const store = configureStore({
   reducer: {
     user: userReducer,
-    [cryptoApiSlice.reducerPath]: cryptoApiSlice.reducer,
-    [binanceApiSlice.reducerPath]: binanceApiSlice.reducer,
-    [marketDataApi.reducerPath]: marketDataApi.reducer,
-    [coinDataApi.reducerPath]: coinDataApi.reducer,
-    [сoinsListWithMarketDataApiScile.reducerPath]: сoinsListWithMarketDataApiScile.reducer,
-    [сoinTickersByIdApiSlice.reducerPath]: сoinTickersByIdApiSlice.reducer,
     [coinsApiSlice.reducerPath]: coinsApiSlice.reducer,
     [globalApiSlice.reducerPath]: globalApiSlice.reducer
 
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(
-      binanceApiSlice.middleware, 
-      cryptoApiSlice.middleware, 
-      marketDataApi.middleware, 
-      coinDataApi.middleware,
-      сoinsListWithMarketDataApiScile.middleware,
-      сoinTickersByIdApiSlice.middleware,
       coinsApiSlice.middleware,
       globalApiSlice.middleware
     ),
