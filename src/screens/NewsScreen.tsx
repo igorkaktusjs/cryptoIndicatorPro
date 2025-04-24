eedd995cbf95431ed60ff0e419a760938f94d0fa
igor <igorkaktus@gmail.com>
2025-04-24 21:10:36 +0100

    Added NewsScreen with CryptoPanic API

--- src/screens/NewsScreen.tsx
+++ src/screens/NewsScreen.tsx
@@ -1,10 +1,15 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import NewsList from '../components/NewsComponentsScreen/NewsList';
+import NewsFilterTabs from '../components/NewsComponentsScreen/NewsFilterTabs';
+import NewsCard from '../components/NewsComponentsScreen/NewsCard';
 
 const NewsScreen = () => {
   return (
     <View>
-      <Text>NewsScreen</Text>
+      <NewsFilterTabs/>
+      <NewsCard/>
+      <NewsList/>
     </View>
   )
 }
